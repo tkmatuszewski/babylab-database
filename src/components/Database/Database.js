7bb5c7625f30b8212dbc4ff2d7a2218e7476b9a0
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-07-12 00:24:59 +0200

    Database main section

--- src/components/Database/Database.js
+++ src/components/Database/Database.js
@@ -1,9 +1,13 @@
 import React from "react";
 import DatabaseHeader from "../DatabaseHeader/DatabaseHeader";
+import DatabaseMain from "../DatabaseMain/DatabaseMain";
 
 function Database() {
     return (
-        <DatabaseHeader/>
+        <>
+            <DatabaseHeader/>
+            <DatabaseMain/>
+        </>
     )
 }
 
