d1473eb8f06ed86f464acd518331d8068a3c4326
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-09-11 00:10:35 +0200

    ProjectsItem flat menu added

--- src/components/ProjectsItem/ProjectsItem.js
+++ src/components/ProjectsItem/ProjectsItem.js
@@ -1,18 +1,25 @@
 import React from "react";
 
 const ProjectItem = (props) => {
+
     const projectData = props.projectInfo
+
     return (
         <li className="projects__container__list__element">
             <div className="projects__container__list__element__container">
                 <h4>{projectData.name}</h4>
                 <p className="projects__container__list__element__description">
+                    <strong>O projekcie</strong>
                     <span>{projectData.description}</span>
                 </p>
                 <span
-                    className="projects__container__list__element__participants">Aktualna liczba dzieci w projekcie : {projectData.participants}</span>
+                    className="projects__container__list__element__participants">Dzieci w projekcie : {projectData.participants}</span>
                 <span
                     className="projects__container__list__element__meetings">Liczba spotkań w projekcie : {projectData.meetingCount}</span>
+                <ul className="projects__container__list__element__menu">
+                    <li className="projects__container__list__element__menu__element">Edytuj</li>
+                    <li className="projects__container__list__element__menu__element">Usuń</li>
+                </ul>
             </div>
         </li>
     )
