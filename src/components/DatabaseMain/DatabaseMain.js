47b617e0dda21933e86a5bfb2467dff5571cd36c
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-09-22 01:38:50 +0200

    DatabaseMain class component changed to functional component

--- src/components/DatabaseMain/DatabaseMain.js
+++ src/components/DatabaseMain/DatabaseMain.js
@@ -1,96 +1,70 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 import AddNewForm from "../AddNewForm/AddNewForm";
 import Projects from "../Projects/Projects";
 import Other1 from "../Other1/Other1";
 import Other2 from "../Other2/Other2";
 import DatabaseData from "../DatabaseData/DatabaseData";
+import MenuItem from "../MenuItem/MenuItem";
 
 const classNames = require('classnames');
 
 
-class DatabaseMain extends Component {
+const DatabaseMain = () => {
 
-    state = {
-        showForm: false,
-        showMenu: false,
-        showOption: null,
-        showDatabase: true
+    const [showMenu, setShowMenu] = useState(false)
+    const [showOption, setShowOption] = useState("Home")
+
+    const handleClickHamburger = (state) => {
+        return setShowMenu(!state);
     }
 
-    handleClickHamburger = (state) => {
-        this.setState({showMenu: !state})
+    const handleMenuOption = (option) => {
+        setShowMenu(false)
+        setShowOption(option)
     }
 
-    handleMenuOption = (option) => {
-        this.setState({
-            showOption: option,
-            showMenu: false,
-            showDatabase: false
-        })
-    }
-
-    handleCloseModal = () => {
-        this.setState({
-            showOption: null,
-            showDatabase: true,
-            showMenu: false
-        })
+    const handleCloseModal = () => {
+        setShowOption("Home")
     };
 
-    render() {
-        const hamburger = classNames({
-            "database__main__control__hamburger": !this.state.showMenu,
-            "database__main__control__hamburger__active": this.state.showMenu
-        })
-        const menu = classNames({
-            "database__main__control__nav": !this.state.showMenu,
-            "database__main__control__nav__active": this.state.showMenu
-        })
-        return (
-            <section className="database__main">
-                <button className={hamburger}
-                        onClick={() => this.handleClickHamburger(this.state.showMenu)} />
-                <ul className={menu}>
-                    <div className="database__main__control__container">
-                        <li className="database__main__control__item"
-                            id="Nowy badany"
-                            onClick={(e) => {
-                                this.handleMenuOption(e.target.id)
-                            }}
-                        >Nowy badany
-                        </li>
-                        <li className="database__main__control__item"
-                            id="Projekty"
-                            onClick={(e) => {
-                                this.handleMenuOption(e.target.id)
-                            }}>Projekty
-                        </li>
-                        <li className="database__main__control__item"
-                            id="Inne 1"
-                            onClick={(e) => {
-                                this.handleMenuOption(e.target.id)
-                            }}>Inne 1
-                        </li>
-                        <li className="database__main__control__item"
-                            id="Inne 2"
-                            onClick={(e) => {
-                                this.handleMenuOption(e.target.id)
-                            }}>Inne 2
-                        </li>
-                    </div>
-                </ul>
-                    {this.state.showOption === 'Projekty' ?
-                        <Projects handleCloseModal={this.handleCloseModal} /> : null}
-                <div className="database__main__container">
-                    {this.state.showOption === 'Nowy badany' ?
-                        <AddNewForm handleCloseModal={this.handleCloseModal} /> : null}
-                    {this.state.showOption === 'Inne 1' ? <Other1 handleCloseModal={this.handleCloseModal} /> : null}
-                    {this.state.showOption === 'Inne 2' ? <Other2 handleCloseModal={this.handleCloseModal} /> : null}
-                    {this.state.showDatabase ? <DatabaseData /> : null}
+    const hamburger = classNames({
+        "database__main__control__hamburger": !showMenu,
+        "database__main__control__hamburger__active": showMenu
+    })
+    const menu = classNames({
+        "database__main__control__nav": !showMenu,
+        "database__main__control__nav__active": showMenu
+    })
+    return (
+        <section className="database__main">
+            <button className={hamburger}
+                    onClick={() => handleClickHamburger(showMenu)} />
+            <ul className={menu}>
+                <div className="database__main__control__container">
+                    <MenuItem name={"Home"}
+                              handleMenuOption={handleMenuOption} />
+                    <MenuItem name={"Nowy badany"}
+                              handleMenuOption={handleMenuOption} />
+                    <MenuItem name={"Projekty"}
+                              handleMenuOption={handleMenuOption} />
+                    <MenuItem name={"Inne 1"}
+                              handleMenuOption={handleMenuOption} />
+                    <MenuItem name={"Inne 2"}
+                              handleMenuOption={handleMenuOption} />
                 </div>
-            </section>
-        )
-    }
+            </ul>
+            {showOption === 'Projekty' ?
+                <Projects handleCloseModal={handleCloseModal} /> : null}
+            <div className="database__main__container">
+                {showOption === 'Home' ? <DatabaseData /> : null}
+                {showOption === 'Nowy badany' ?
+                    <AddNewForm handleCloseModal={handleCloseModal} /> : null}
+                {showOption === 'Inne 1' ? <Other1 handleCloseModal={handleCloseModal} /> : null}
+                {showOption === 'Inne 2' ? <Other2 handleCloseModal={handleCloseModal} /> : null}
+            </div>
+        </section>
+    )
 }
+
 
 export default DatabaseMain
