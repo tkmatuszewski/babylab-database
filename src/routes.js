d80a3faf567bb7e7da96457003823977c2c60473
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-10-20 21:22:50 +0200

    Routing paths saved as constants

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+export const signIn = "/signin"
+export const signUp = "/signup"
+export const home = "/"
+export const projects = "/projects"
+export const newParticipantForm = "/new"
+export const resetPassword = "/reset"
+export const example1 = "/example1"
+export const example2 = "/example2"
+
