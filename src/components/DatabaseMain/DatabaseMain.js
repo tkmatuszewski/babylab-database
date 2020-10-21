98fe6e0c09057f77358fcfd317169faf7f3f8eb1
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-10-21 21:59:03 +0200

    DatabaseMain refactored and simplified

--- src/components/DatabaseMain/DatabaseMain.js
+++ src/components/DatabaseMain/DatabaseMain.js
@@ -1,70 +1,26 @@
-import React, {useState} from "react";
-import AddNewForm from "../AddNewForm/AddNewForm";
-import Projects from "../Projects/Projects";
-import Other1 from "../Other1/Other1";
-import Other2 from "../Other2/Other2";
+import React from "react";
+import {withRouter} from "react-router-dom";
+import DatabaseHeader from "../DatabaseHeader/DatabaseHeader";
+import Navigation from "../Navigation/Navigation";
+import {getCurrentUserName} from "../Firebase/FirebaseAuth";
+import {signIn} from "../../routes";
 import DatabaseData from "../DatabaseData/DatabaseData";
-import MenuItem from "../MenuItem/MenuItem";
 
-const classNames = require('classnames');
+const DatabaseMain = ({history}) => {
 
-
-const DatabaseMain = () => {
-
-    const [showMenu, setShowMenu] = useState(false)
-    const [showOption, setShowOption] = useState("Home")
-
-    const handleClickHamburger = (state) => {
-        return setShowMenu(!state);
+    if (!getCurrentUserName()) {
+        history.replace(signIn)
+        return null
     }
 
-    const handleMenuOption = (option) => {
-        setShowMenu(false)
-        setShowOption(option)
-    }
-
-    const handleCloseModal = () => {
-        setShowOption("Home")
-    };
-
-    const hamburger = classNames({
-        "database__main__control__hamburger": !showMenu,
-        "database__main__control__hamburger__active": showMenu
-    })
-    const menu = classNames({
-        "database__main__control__nav": !showMenu,
-        "database__main__control__nav__active": showMenu
-    })
     return (
-        <section className="database__main">
-            <button className={hamburger}
-                    onClick={() => handleClickHamburger(showMenu)} />
-            <ul className={menu}>
-                <div className="database__main__control__container">
-                    <MenuItem name={"Home"}
-                              handleMenuOption={handleMenuOption} />
-                    <MenuItem name={"Nowy badany"}
-                              handleMenuOption={handleMenuOption} />
-                    <MenuItem name={"Projekty"}
-                              handleMenuOption={handleMenuOption} />
-                    <MenuItem name={"Inne 1"}
-                              handleMenuOption={handleMenuOption} />
-                    <MenuItem name={"Inne 2"}
-                              handleMenuOption={handleMenuOption} />
-                </div>
-            </ul>
-            {showOption === 'Projekty' ?
-                <Projects handleCloseModal={handleCloseModal} /> : null}
-            <div className="database__main__container">
-                {showOption === 'Home' ? <DatabaseData /> : null}
-                {showOption === 'Nowy badany' ?
-                    <AddNewForm handleCloseModal={handleCloseModal} /> : null}
-                {showOption === 'Inne 1' ? <Other1 handleCloseModal={handleCloseModal} /> : null}
-                {showOption === 'Inne 2' ? <Other2 handleCloseModal={handleCloseModal} /> : null}
-            </div>
+        <section className="databaseMain">
+            <DatabaseHeader />
+            <Navigation>
+                <DatabaseData />
+            </Navigation>
         </section>
     )
 }
 
-
-export default DatabaseMain
+export default withRouter(DatabaseMain);
