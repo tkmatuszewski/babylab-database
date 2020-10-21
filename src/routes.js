442aedc1a589f50e13f4aa8a9653194351f94e72
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-10-21 22:33:25 +0200

    route constants

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+export const signIn = "/signin"
+export const signUp = "/signup"
+export const home = "/"
+export const projects = "/projects"
+export const projectsAddNew = "/projects/addnew"
+export const newParticipantForm = "/new"
+export const resetPassword = "/reset"
+export const example1 = "/example1"
+export const example2 = "/example2"
+
