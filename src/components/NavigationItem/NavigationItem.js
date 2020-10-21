cc5bd154992f0e27ac059e2f6dd4f6bea8db2652
Tomasz Matuszewski <tkmatuszewski@gmail.com>
2020-10-21 21:47:47 +0200

    NavigationItem redesigned

--- src/components/NavigationItem/NavigationItem.js
+++ src/components/NavigationItem/NavigationItem.js
@@ -1,11 +1,21 @@
 import React from "react";
+import {Link, withRouter} from "react-router-dom";
 
-const NavigationItem = ({handleMenuOption, name}) => {
-    return <li className="database__main__control__item"
-               id={name}
-               onClick={()=> handleMenuOption(name)}
-    >{name}
-    </li>
+const NavigationItem = ({name, value, history, closeNav}) => {
+
+    const clickHandler = (value) => {
+        closeNav(false)
+        return  history.replace(value)
+    }
+
+    return (
+        <li className="navigationItem"
+            id={name}>
+            <Link to={value} onClick={ ()=> {clickHandler(value)}}>
+                {name}
+            </Link>
+        </li>
+    )
 }
 
-export default NavigationItem
+export default withRouter(NavigationItem);
